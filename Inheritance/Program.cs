8612545ac2e232a2ecca31af56c515ce36c3075a
Kevin March <marchkevin78@gmail.com>
2020-10-01 18:45:34 -0400

    FinishedInheritanceExercise

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,6 +6,7 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+
             // TODO Be sure to follow best practice when creating your classes
 
             // Create a class Animal
@@ -20,7 +21,31 @@ namespace Inheritance
             // give this class 4 members that are specific to Reptile
             // Set this class to inherit from your Animal Class
 
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
 
+            Console.WriteLine($"My bird has {myBird.WingColor} colored wings.\n" +
+                $"If you are wondering if my bird is able to fly and/or will migrate, the answers are {myBird.CanFly} and {myBird.WillMigrate}!\n" +
+                $"The length of my bird's beak is {myBird.BeakLength} inches.\n");
+
+
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true
+            };
+
+            Console.WriteLine($"Our lizard's habitat is in the {lizard.Habitat}.\n" +
+                $"You are probably wondering a few other attributes about our lizard. \n" +
+                $"Is our lizard cold blooded? {lizard.IsColdBlooded}\n" +
+                $"Is our lizard scaly? {lizard.IsScaly}\n" +
+                $"Can our lizard grow a tail? {lizard.CanGrowTail}\n"
+                );
 
 
             /*Create an object of your Bird class
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Inheritance
+{
+    public class Reptile : Animal
+    {
+        public Reptile()
+        {
+        }
+
+        public bool IsColdBlooded { get; set; }
+        public bool IsScaly { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+    }
+}
